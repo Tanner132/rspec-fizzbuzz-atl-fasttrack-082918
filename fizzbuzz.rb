d9509cac9597c42f2df4638e08b50d6539b84a25
Tanner Strickland <tannerstrickland2000@yahoo.com>
2018-09-30 13:53:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+# See README.md for instructions on how to do 
+ def fizzbuzz(number)
+   returnString= ""
+   if number % 3 == 0 && number % 5 == 0
+     
+      returnString = "FizzBuzz"
+      
+   elsif number % 5 == 0
+    
+     returnString = "Buzz"
+     
+   elsif number % 3 == 0
+   
+     returnString = "Fizz"
+     
+    else returnString = nil
+   end
+ end
